2643f95772191c47794d5ba47d744f5e2e8558e4
suryanshy11107 <suryanshy11107@gmail.com>
2026-07-29 10:57:26 +0530

    Time: 0 ms (100%), Space: 43 MB (63.23%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -20,8 +20,8 @@ class Solution {
             }
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
-        if(root.val == 0 && root.left == null && root.right == null)
-        {return null;
+        if(root.val == 0 && root.left == null && root.right == null){
+            return null;
         }
         return root;
     }
